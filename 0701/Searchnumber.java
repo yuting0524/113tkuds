d0686ab36ab69553581e559fd23a3c4c6cf5fab9
yuting0524 <yutingwu050089@gmail.com>
2025-07-01 10:37:39 +0800

    searchnumber

---
+++ 0701/Searchnumber.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Searchnumber{
+    public static void main(String[] args){
+        int[]s = {1,3,5,7,9,11,13};
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("請輸入要查找的數字");
+        int x = scanner.nextInt();
+
+        int left = 0;
+        int right = s.length - 1;
+        boolean found = false;
+
+        while(left <= right){
+            int mid = (left + right)/2;
+            System.out.println("比比對中:x="+x+",中間元素="+s[mid]);
+
+            if(s[mid] == x){
+                found = true;
+                break;
+            }
+            else if (s[mid] < x){
+                left = mid + 1;
+            }
+            else{
+                right = mid - 1;
+            }
+            if(found){
+                System.out.println("Yes");
+            }
+            else{
+                 System.out.println("No");
+            }
+        }
+    }
+}
